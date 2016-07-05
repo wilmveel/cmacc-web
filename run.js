d2ea0cf9a4d9f6045b76e8c2435fa1da70687b7f
Willem Veelenturf <willem.veelenturf@gmail.com>
2016-07-05 16:56:33 +0200

    add bower

--- run.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var marked = require('marked');
-
-var text = fs.readFileSync('./doc/location.md');
-
-console.log(marked(text.toString()));
-
-fs.writeFileSync('./index.html', marked(text.toString()));
