b36bc9797cf9bbcfd1b9142ef2b14c3241c91c53
Willem Veelenturf <willem.veelenturf@gmail.com>
2016-07-24 14:40:39 +0200

    make IPFS integration

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
+var url = require('url');
 var express = require('express');
 
 var webpack = require("webpack");
 var webpackMiddleware = require("webpack-dev-middleware");
 var webpackConfig = require('./webpack.config');
+var proxy = require('proxy-middleware');
 
 var port = process.env.PORT || 3000;
 
 var app = express();
 
+// Static
 app.use("/index.html", express.static(__dirname + '/index.html'));
-
 app.use('/doc', express.static(__dirname + '/doc'));
 app.use('/web', express.static(__dirname + '/web'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
-
+// Webpack
 app.use(webpackMiddleware(webpack(webpackConfig), {
     publicPath: "/assets/"
 }));
+
+// IPFS
+app.use('/ipfs', proxy(url.parse('https://gateway.ipfs.io/ipfs/')));
+
 
 app.listen(port, function () {
     console.log('Example app listening on port 3000!');
--- src/import.js
+++ src/import.js
@@ -1,29 +1,44 @@
 var fs = require("fs");
+var url = require("url");
 var request = require("request");
 
 var imp = {
 
+
     readFile : function(file, callback){
 
-        if(!fs.readFile){
+        var parse = url.parse(file);
+
+        if(XMLHttpRequest){
+
+            var call = '';
+            if(parse.protocol === "ipfs:")
+                call += '/ipfs/' + file.replace('ipfs://', '')
+
+            call += parse.path || '/'
+
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (xhttp.readyState == 4 && xhttp.status == 200) {
                     callback(null, xhttp.responseText)
                 }
             };
-            xhttp.open("GET", file, true);
+            xhttp.open("GET", call, true);
             xhttp.send();
             return;
         }
 
-        if(/^http\:\/\//.test(file)){
+        if(parse.protocol === "http:"){
             request(file, function (error, response, body) {
                 if (!error && response.statusCode == 200) {
                     callback(null, body)
                 }
-            })
+            });
             return;
+        }
+
+        if(parse.protocol === "ipfs:"){
+
         }
 
         fs.readFile(file, 'utf8', callback);
