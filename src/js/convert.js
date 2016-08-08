a084a5a2dec4a44327b1d5246cce44875a5b8c84
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2016-08-08 23:25:45 +0200

    refactor

--- src/js/convert.js
+++ src/js/convert.js
@@ -3,32 +3,25 @@ var path = require('path');
 
 var regex = require('../regex');
 
-var convert = function (file, callback) {
-    var res = ''
+function convert(file) {
+    var res = '';
 
-    fs.readFile(file, 'utf8', function (err, text) {
+    var text = fs.readFileSync(file, 'utf8');
 
-        if (err)
-            return callback(err)
-
-        var md = text.replace(regex.REGEX_VARIABLE, function (match, key, ref, val) {
-            res += 'var ' + key + ' = {' + '\n';
-            if (ref) res += '\tfile : "' + ref + '",\n';
-            if (val) res += '\tvars : ' + val + '\n';
-            res += '};\n\n';
-            return ''
-        });
-
-        res += 'module.exports = "' + md.replace(/\n/g, '\\n') + '";'
-
-        callback(null, res)
-
+    var md = text.replace(regex.REGEX_VARIABLE, function (match, key, ref, val) {
+        res += 'var ' + key + ' = {' + '\n';
+        if (ref) res += '\tfile : "' + ref + '",\n';
+        if (val) res += '\tvars : ' + val + '\n';
+        res += '};\n\n';
+        return ''
     });
+
+    res += 'module.exports = "' + md.replace(/\n/g, '\\n') + '";';
+
+    return res;
 }
 
+var js = convert(path.join(__dirname + '/../../doc/test.md'));
+console.log(js);
+
 module.exports = convert;
-
-
-convert('../../doc/test.md', function (err, js) {
-    console.log(js)
-});
